4b037206529c6ba9c94262a7af6a2ae7e947acbb
Philip Carrington <philip.carrington@gmail.com>
2020-07-23 15:17:12 +0000

    various bits added

--- models/customer_details_incremental.sql
+++ models/customer_details_incremental.sql
@@ -10,6 +10,6 @@ from homework__loading.customer_stage
 {% if is_incremental() %}
 
   -- this filter will only be applied on an incremental run
-  where parse_date('%d/%m/%Y', load_date) > (select max(event_time) from {{ this }})
+  where parse_date('%d/%m/%Y', load_date) > (select max(parse_date('%d/%m/%Y', load_date)) from {{ this }})
 
 {% endif %}
