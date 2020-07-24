b1fea5f7fb2c77a57bc699a4b900acc011258c09
Philip Carrington <philip.carrington@gmail.com>
2020-07-24 07:00:12 +0000

    working snapshots

--- snapshots/customer_details_snapshot.sql
+++ snapshots/customer_details_snapshot.sql
@@ -1,14 +1,25 @@
 {% snapshot customer_data_snapshot_timestamp %}
-
+    -- dbt snapshot --select customer_data_snapshot_timestamp
     {{
         config(
+          target_database='cw-dbt-homework',
           target_schema='homework',
-          strategy='timestamp',
           unique_key='customer_id',
-          updated_at='load_date',
+
+          strategy='timestamp',          
+          updated_at='loaded_date',
         )
     }}
 
-    select * from {{ source('homework__loading', 'customer_stage') }}
+    select * 
+    from homework__loading.customer_to_process
+    where loaded_date = (
+                          select min(loaded_date)
+                          from homework__loading.customer_to_process
+                          where loaded_date > (
+                                                select coalesce(max(dbt_valid_from), '1900-01-01')
+                                                from homework.customer_data_snapshot_timestamp
+                                              )
+                        )
 
 {% endsnapshot %}
